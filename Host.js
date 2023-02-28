ecc2eacf5a77ff8fadac4447cb8684c6aedbd29b
Joshua Adewale <josh.jobadewale@gmail.com>
2023-02-28 04:36:36 +0100

    update

--- Host.js
+++ Host.js
@@ -1,91 +1,107 @@
-const socket = new WebSocket("ws://127.0.0.1:5500");
+const webSocket = new WebSocket("ws://127.0.0.1:3000")
 
-WebSocket.onmessage = (event) => {
-  handleSignalingData(JSON.parse(event.data))
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
 }
 
-function handleSignalingData(data) {
-  switch(data,type) {
-    case "answer":
-      peerConn.setRemoteDescription(data.answer)
-      break
-    case "candidate":
-      peerConn.addIceCandidate(data.candidate)
-
-  }
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "answer":
+            peerConn.setRemoteDescription(data.answer)
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
 }
 
-let username;
+let username
 function sendUsername() {
-  username = document.getElementById("username").value
-  SendData({
-    type: "store_data"
-  });
-};
-function SendData(data) {
-  data.username = username
-  WebSocket.send(JSON.stringify(data));
-};
 
-let localStream;
+    username = document.getElementById("username-input").value
+    sendData({
+        type: "store_user"
+    })
+}
+
+function sendData(data) {
+    data.username = username
+    webSocket.send(JSON.stringify(data))
+}
+
+
+let localStream
 let peerConn
 function startCall() {
-  document.getElementById("video-call-div").style.display = "inline";
-  
-  navigator.getUserMedia({
-    video: {
-      frameRate: 24,
-      width: {
-        min: 480, ideal: 720, max: 1280
-      },
-      aspectRatio: 1.333
-    },
-    audio: true
-  }, (stream) => {
-    localStream = stream
-    document.getElementById("local-video").srcObject = localStream
-    
-    let configuration = {
-      iceServers: [
-        {
-          "urls": ["stun:stun.l.google.com:19302","stun:stun1.l.google.com:19302","stun:stun2.l.google.com:19302"]
-        } 
-      ]
-    }
+    document.getElementById("video-call-div")
+    .style.display = "inline"
 
-    peerConn = RTCPeerConnection(configuration)
-    peerConn.addStream(localStream)
+    navigator.getUserMedia({
+        video: {
+            frameRate: 24,
+            width: {
+                min: 480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.33333
+        },
+        audio: true
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream
 
-    peerConn.onaddstream = (e) => {
-      document.getElementById("remote-video").srcObject = e.stream
-    }
+        let configuration = {
+            iceServers: [
+                {
+                    "urls": ["stun:stun.l.google.com:19302", 
+                    "stun:stun1.l.google.com:19302", 
+                    "stun:stun2.l.google.com:19302"]
+                }
+            ]
+        }
 
-    peerConn.onicecandidte = ((e) => {
-      if(e.candidate == null)
-        return
+        peerConn = new RTCPeerConnection(configuration)
+        peerConn.addStream(localStream)
 
-      SendData({
-        type: "store_candidate",
-        candidate: e.candidate
-      })  
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote-video")
+            .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            sendData({
+                type: "store_candidate",
+                candidate: e.candidate
+            })
+        })
+
+        createAndSendOffer()
+    }, (error) => {
+        console.log(error)
     })
-
-    createAndSendOffer()
-    
-  }, (error) => {
-    console.log("error");
-  })
 }
 
 function createAndSendOffer() {
-  peerConn.createOffer((offer) => {
-    SendData({
-      type: "store_offer",
-      offer: offer
-    })
+    peerConn.createOffer((offer) => {
+        sendData({
+            type: "store_offer",
+            offer: offer
+        })
 
-    peerConn.setLocalDescription(offer) 
-  }, (error) => {
-    console.log("error");
-  })
+        peerConn.setLocalDescription(offer)
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+let isAudio = true
+function muteAudio() {
+    isAudio = !isAudio
+    localStream.getAudioTracks()[0].enabled = isAudio
+}
+
+let isVideo = true
+function muteVideo() {
+    isVideo = !isVideo
+    localStream.getVideoTracks()[0].enabled = isVideo
 }
